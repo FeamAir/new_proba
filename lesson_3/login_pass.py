451e1f34a5764d279f104ac0128e7e7e9e732af2
Feam <maryking.textile@gmail.com>
2021-11-18 22:20:15 +0200

    number

--- lesson_3/login_pass.py
+++ lesson_3/login_pass.py
@@ -1,1 +1,6 @@
-message = int (input  ("enter password \n"))
+a = int(input("enter password \n"))
+b = "not correct, try again"
+if a == 1111:
+    print("password good")
+else:
+    print(b)
---
+++ lesson_3/peremennie.py
@@ -0,0 +1,28 @@
+# How can I swap the values two numbers?
+a = 2; b = 8
+a = a * b
+b = a // b
+a = a // b
+print("a =", a, "b=", b)
+print("b=", b, "a =", a)
+
+a = 2; b = 8
+a = a + b
+b = a - b
+a = a - b
+print("a=", a, "b=", b)
+print("b=", b, "a =", a)
+
+a = 2; b = 8
+a = b // a
+b = b // a
+a = a * b
+print("a=", a, "b=", b)
+print("b=", b, "a =", a)
+
+a = 2; b = 8
+a = b ^ a
+b = b ^ a
+a = a ^ b
+print("a=", a, "b=", b)
+print("b=", b, "a =", a)
