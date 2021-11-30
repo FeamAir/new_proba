2c0373676117e8184492863e19b8d3ed90132b40
Feam <maryking.textile@gmail.com>
2021-11-30 20:57:32 +0200

    correct  probel

--- lesson_4/two numberes.py
+++ lesson_4/two numberes.py
@@ -2,13 +2,13 @@ a = int(input("Enter numbers \n"))
 a1 = a % 10
 a = a // 10
 k = 0
-while a>0:
+while a > 0:
    a2 = a % 10
-   if a1==a2:
-      k+=1
-   a = a //10
+   if a1 == a2:
+      k += 1
+   a = a // 10
    a1 = a2
-if k>0: print("Yes")
-else: print("No")
-
-
+if k > 0:
+   print("Yes")
+else:
+   print("No")
