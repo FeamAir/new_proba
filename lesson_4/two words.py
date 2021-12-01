25fd78881599c49f7e580021f1afbe49f5e0b3f5
Feam <maryking.textile@gmail.com>
2021-12-01 13:10:38 +0200

    find only one space

--- lesson_4/two words.py
+++ lesson_4/two words.py
@@ -1,11 +1,11 @@
-say = input("Enter two words \n")
-a = say.split(' ')
-c = 2
-while c == len(a):
-    spaceindex = say.find(' ')
-    w1 = say[:int(spaceindex) + 1][::-1]
-    w2 = say[int(spaceindex):][::-1]
-    print(w1.title(), w2.title())
-    break
-else:
-    print("try again")
+while True:
+    say = input("Enter two words \n")
+    a = say.count(" ")
+    if a == 1:
+        words = say.find(' ')
+        w1 = say[:int(words) + 1][::-1]
+        w2 = say[int(words):][::-1]
+        print(w1.title(), w2.title())
+        break
+    else:
+        print("Try again")
