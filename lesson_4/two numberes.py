666ac8d6eea2f0f071504e12767b72cd9e77571a
Feam <maryking.textile@gmail.com>
2021-12-01 16:40:05 +0200

    correct space

--- lesson_4/two numberes.py
+++ lesson_4/two numberes.py
@@ -3,12 +3,12 @@ a1 = a % 10
 a = a // 10
 k = 0
 while a > 0:
-   a2 = a % 10
-   if a1 == a2:
-      k += 1
-   a = a // 10
-   a1 = a2
+    a2 = a % 10
+    if a1 == a2:
+        k += 1
+    a = a // 10
+    a1 = a2
 if k > 0:
-   print("Yes")
+    print("Yes")
 else:
-   print("No")
+    print("No")
