20206e57658ee95e3a07c182ee42575e923b37e9
Feam <maryking.textile@gmail.com>
2021-12-03 15:57:09 +0200

    correct back numbers

--- lesson_5/pyramid.py
+++ lesson_5/pyramid.py
@@ -3,10 +3,10 @@ c = 10
 while True:
     say = int(input("enter number from 3 to 9 \n"))
     if b < say < c:  # проверяем правильность выполнения условия
-        for i in range(1, say + 2): # перебераем от одного к задоному числу в два шага
-            for a in range(1, i - 1): #перебирае от одного c  минус один шагом
+        for i in range(1, say + 2):  # перебераем от одного к задоному числу в два шага
+            for a in range(1, i - 1):  # перебирае от одного c  минус один шагом
                 print(a, end="")
-            for a in range(i - 1, 0, -1): #перебирает числа в обратном порядке
+            for a in range(i - 1, 0, -1):  # перебирает числа в обратном порядке
                 print(a, end="")
             print()
         break
