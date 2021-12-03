e2c27ea816f539c94e1069c80a797a841983a6fe
Feam <maryking.textile@gmail.com>
2021-12-03 18:40:58 +0200

    correct bag symbol

--- lesson_5/lucky_number.py
+++ lesson_5/lucky_number.py
@@ -5,15 +5,20 @@ number = random.randint(1, 100)
 cnt = 0
 print(f"Good {name},I create for you a number from 1 to 100, good luck!!!")
 while True:
-    say = int(input(f" {name} enter your number \n"))
+    say = input(f" {name} enter your number \n")
     cnt += 1
     text = "your number {luck} is bigger".format(luck=say)
     text1 = "your number {luck} is less".format(luck=say)
-    if say > number:  # если число введенное пользоваелем больше рендомного
-        print(text)
-    if say < number:  # если число введенное пользоваелем меньше рендомного
-        print(text1)
-    if say == number:  # если число введенное пользоваелем совпала с рендомным числом
-        break
-if say == number:
+    if say.isdigit():
+        say1 = int(say)
+        print("")
+        if say1 > number:  # если число введенное пользоваелем больше рендомного
+            print(text)
+        if say1 < number:  # если число введенное пользоваелем меньше рендомного
+            print(text1)
+        if say1 == number:  # если число введенное пользоваелем совпала с рендомным числом
+            break
+    else:
+        print("incorrect symbol, try again")
+if say1 == number:
     print(f"{name}, you win. Number of attempts = {cnt}")
