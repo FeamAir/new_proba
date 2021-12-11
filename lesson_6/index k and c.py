fb73364634ed2e01973f63d81b93c8ccf0fda9a5
Feam <maryking.textile@gmail.com>
2021-12-11 09:22:52 +0200

    change

--- lesson_6/index k and c.py
+++ lesson_6/index k and c.py
@@ -5,9 +5,24 @@ print(say)
 k = int(input("enter index"))
 c = int(input("enter change index"))
 d = int(input("enter add number"))
-say = say[:k]+[c]+say[k:]
+for i in range(k, len(say) - 1):
+    say.append(c)
+    say[-1], say[k] = say[k], say[-1]
+    break
 print(say)
 say.append(d)
-print(say)
 say.sort()
 print(say)
+
+
+# say = [0, 1, 2, 3, 4, 5, 6]
+# k = (4)
+# print(type(k))
+# c = (5)
+# say1 = say + [c]
+# print(say1)
+# for i in range(k, len(say1)-1):
+#     say1[-1],say1[k] = say1[k],say1[-1]
+# print(say1)
+
+# say1[-1],say1[k] = say1[k],say1[-1]
