e34658bed384c97d510eb021e57c0b5fbc458e50
Feam <maryking.textile@gmail.com>
2021-12-11 21:56:55 +0200

    change again

--- lesson_6/index k and c.py
+++ lesson_6/index k and c.py
@@ -4,12 +4,9 @@ say = [random.randint(1, 10) for i in range(10)]
 print(say)
 k = int(input("enter index"))
 c = int(input("enter change index"))
-d = int(input("enter add number"))
-for i in range(k, len(say) - 1):
-    say.append(c)
-    say[-1], say[k] = say[k], say[-1]
-    break
+say.append(c)
+for a in range(len(say) - 1, k, -1):
+    say[a], say[a - 1] = say[a - 1], say[a]
 print(say)
-say.append(d)
 say.sort()
 print(say)
