929a7ca3f1ffa0402f8549934366fd7dbfddfb54
Feam <maryking.textile@gmail.com>
2021-12-13 21:21:12 +0200

    often_word

---
+++ lesson_7/often word.py
@@ -0,0 +1,15 @@
+dict = {}
+txt = """
+Стоишь на берегу и чувствуешь соленый запах ветра, что веет с моря, и веришь,
+что свободен ты и жизнь лишь началась, и губы жжет подруги поцелуй, пропитанный слезой!
+"""
+print(txt)
+for words in txt.split():
+    dict[words] = dict.get(words, 0) + 1
+#        or с учетом запятых
+# for words in txt.replace(',', ' ,').split():
+#     dict[words] = dict.get(words, 0) + 1
+print(dict)
+max_count = max(dict.values())
+most_frequent = [k for k, v in dict.items() if v == max_count]
+print(max(most_frequent))
