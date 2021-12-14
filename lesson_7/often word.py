83d0d5d9b0b6e5011555f75cfd9eb67cc74fcd5a
Feam <maryking.textile@gmail.com>
2021-12-14 13:58:52 +0200

    no_correct

--- lesson_7/often word.py
+++ lesson_7/often word.py
@@ -1,7 +1,7 @@
 dict = {}
 txt = """
 Стоишь на берегу и чувствуешь соленый запах ветра, что веет с моря, и веришь,
-что свободен ты и жизнь лишь началась, и губы жжет подруги поцелуй, пропитанный слезой!
+что свободен ты и жизнь лишь началась, и губы жжет жжет жжет жжет жжет подруги поцелуй, пропитанный слезой!
 """
 print(txt)
 for words in txt.replace(',', ' ,').split():
@@ -9,7 +9,7 @@ for words in txt.replace(',', ' ,').split():
 print(dict)
 max_cnt = max(dict.values())
 often = [k for k, v in dict.items() if v == max_cnt]
-print("You have = ", often)
+# print("You have = ", often)
 for i in range(len(often)):
     if i > 1:
         d = input("what words?? \n")
