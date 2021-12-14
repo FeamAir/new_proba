f2630853c3a4cf8d42c2ca517d861bf4b2274cab
Feam <maryking.textile@gmail.com>
2021-12-14 12:14:49 +0200

    correct

--- lesson_7/often word.py
+++ lesson_7/often word.py
@@ -1,15 +1,16 @@
 dict = {}
 txt = """
-Стоишь на берегу и чувствуешь соленый запах ветра, что веет с моря, и веришь,
-что свободен ты и жизнь лишь началась, и губы жжет подруги поцелуй, пропитанный слезой!
+Стоишь на берегу и чувствуешь соленый запах запах запах запах запах ветра, что веет с моря, и веришь,
+что свободен ты и жизнь лишь началась, и губы жжет жжет жжет жжет жжет подруги поцелуй, пропитанный слезой!
 """
 print(txt)
-for words in txt.split():
+for words in txt.replace(',', ' ,').split():
     dict[words] = dict.get(words, 0) + 1
-#        or с учетом запятых
-# for words in txt.replace(',', ' ,').split():
-#     dict[words] = dict.get(words, 0) + 1
 print(dict)
 max_cnt = max(dict.values())
 often = [k for k, v in dict.items() if v == max_cnt]
-print(max(often))
+print("You have = ", often)
+for i in range(len(often)):
+    if i > 1:
+        d = input("what words?? \n")
+        print("total value", dict[d])
---
+++ lesson_8/set_1.py
@@ -0,0 +1,7 @@
+import random
+
+lst1 = [random.randint(1, 10) for i in range(5)]
+lst = list(lst1)
+print(type(lst))
+# print(len(set(lst1.split())))
+
