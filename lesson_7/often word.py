8b8bc8bc5183cbe24df88a385c1bd5cfff37504e
Feam <maryking.textile@gmail.com>
2021-12-14 15:27:22 +0200

    double_new_correct

--- lesson_7/often word.py
+++ lesson_7/often word.py
@@ -1,15 +1,21 @@
 dict = {}
 txt = """
-Стоишь на берегу и чувствуешь соленый запах ветра, что веет с моря, и веришь,
+Стоишь на берегу и чувствуешь соленый запах запах запах запах запах ветра, что веет с моря, и веришь,
 что свободен ты и жизнь лишь началась, и губы жжет жжет жжет жжет жжет подруги поцелуй, пропитанный слезой!
 """
 print(txt)
+d = " "
 for words in txt.replace(',', ' ,').split():
     dict[words] = dict.get(words, 0) + 1
 print(dict)
 max_cnt = max(dict.values())
 often = [k for k, v in dict.items() if v == max_cnt ]
 print("You have = ", often)
-if len(often) > 1:
+while len(often) > 1:
     d = input("what words?? \n")
+    if d not in dict:
+        print("not found")
+        continue
     print("total value", d, "=", dict[d])
+    break
+
