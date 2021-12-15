b7d2f75f7b3a322421bc9c1d541a8aa1433eb9b3
Feam <maryking.textile@gmail.com>
2021-12-15 15:51:45 +0200

    new_set1

--- lesson_8/set_1.py
+++ lesson_8/set_1.py
@@ -1,7 +1,1 @@
-import random
-
-lst1 = [random.randint(1, 10) for i in range(5)]
-lst = list(lst1)
-print(type(lst))
-# print(len(set(lst1.split())))
-
+print(len(set([int(i) for i in input().split()])))
