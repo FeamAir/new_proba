7d4229f7b141e38bce2eae510fe4b0332f27c2a9
Feam <maryking.textile@gmail.com>
2021-12-15 17:23:06 +0200

    set2_question

--- lesson_8/set_1.py
+++ lesson_8/set_1.py
@@ -1,1 +1,6 @@
+import random
+
 print(len(set([int(i) for i in input().split()])))
+
+# or хочу что бы не только подсчет выводился но и сам список с которого он считает
+# print(len(set([i for i in {random.randint(0, 10) for i in range(10)}])))
