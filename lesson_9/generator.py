f319e2b9381ffd96464c0369b98c856b34d362c8
Feam <maryking.textile@gmail.com>
2021-12-19 11:17:29 +0200

    correct_yield

--- lesson_9/generator.py
+++ lesson_9/generator.py
@@ -1,10 +1,13 @@
 def ger(x, y):
-    for i in range(x, y + 1):
-        if 0 < x < y < 101:
-            print(i, end=" ")
-        else:
-            print("False, only numbers 1-100")
-            return i
+    if 0 < x < y < 101:
+        while x != y:
+            yield x + 1
+            x += 1
+    else:
+        print("False, only numbers 1-100")
 
 
 a = ger(int(input("enter from num \n")), int(input("enter untill num \n")))
+print(next(a), end=" ")
+
+
