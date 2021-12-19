09eb0cad15da42c9b1261d4646edc1cee25c550f
Feam <maryking.textile@gmail.com>
2021-12-19 14:22:34 +0200

    correct_Prime_number

--- lesson_9/generator.py
+++ lesson_9/generator.py
@@ -1,13 +1,18 @@
 def ger(x, y):
     if 0 < x < y < 101:
-        while x != y:
-            yield x + 1
-            x += 1
+            for i in range(x, y):
+                if i % 2 != 0:
+                    yield i
     else:
         print("False, only numbers 1-100")
 
 
 a = ger(int(input("enter from num \n")), int(input("enter untill num \n")))
 print(next(a), end=" ")
+
+
+
+
+
 
 
