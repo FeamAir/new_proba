e00cf8f2ff45aa8ece754ca92eb2851cfcae5e1b
Feam <maryking.textile@gmail.com>
2021-12-19 16:17:27 +0200

    correct

--- lesson_9/generator.py
+++ lesson_9/generator.py
@@ -1,18 +1,36 @@
 def ger(x, y):
     if 0 < x < y < 101:
-            for i in range(x, y):
-                if i % 2 != 0:
-                    yield i
+        for num in range(x, y):
+            if num > 1:
+                for i in range(2, num):
+                    if (num % i) == 0:
+                        break
+                else:
+                    yield num
     else:
         print("False, only numbers 1-100")
 
 
 a = ger(int(input("enter from num \n")), int(input("enter untill num \n")))
 print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
+print(next(a), end=" ")
 
 
-
-
-
-
-
+# a = int(input('enter the lower no.'))
+# b = int(input('enter the higher no.'))
+# print("Prime numbers between", a, "and", b, "are:")
+# for num in range(a, b):
+#     if num > 1:
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 break
+#         else:
+#             print(num)
