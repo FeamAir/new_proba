3cff56e377ea3916a79654b3a2188d80495c59ff
Feam <maryking.textile@gmail.com>
2021-12-21 12:57:47 +0200

    ybral_diapozon

--- lesson_9/generator.py
+++ lesson_9/generator.py
@@ -1,14 +1,11 @@
 def ger(x, y):
-    if 0 < x < y < 101:
-        for num in range(x, y):
-            if num > 1:
-                for i in range(2, num):
-                    if (num % i) == 0:
-                        break
-                else:
-                    yield num
-    else:
-        print("False, only numbers 1-100")
+    for num in range(x, y):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                yield num
 
 
 a = ger(int(input("enter from num \n")), int(input("enter untill num \n")))
