1d043fb9caecb974a5bc45d28bb01b6b74a2d885
Feam <maryking.textile@gmail.com>
2022-01-17 22:10:15 +0200

    correct

--- lesson_16/class_counter.py
+++ lesson_16/class_counter.py
@@ -2,12 +2,22 @@ class Counter:
     """Класс Counter - счетчик, принимает на вход зачения
     "start" и "end", при каждом выводе происходит
      инкрементация +1, а так же обнуления счетчика"""
+
     def __init__(self, start, end):
         self.start = start
         self.end = end
         self.current = self.start
+        self.status = self.current  # << добавил статус
 
-    def increment_count(self):
+    def status_count(self):  # добавил метод  статус
+        if self.current < self.end:
+            self.status = self.current
+            self.current += 1
+            return self.status  # теперь возвращает номера счетчика
+        else:
+            return "Превышено значение счетчика"
+
+    def increment_count(self):  # метод не изминял
         """Фукция инкрементирует значение счетчика,
             а так же следит за границами выполнения"""
         if self.current < self.end:
@@ -16,17 +26,17 @@ class Counter:
         else:
             return "Превышено значение счетчика"
 
-    def refresh_count(self):
+    def refresh_count(self):  # изменил на первоначальный отсчет
         """Обнуления счетчика"""
-        self.current = self.start + 1
-        return self.current
+        self.current = self.start  # изменил на первоначальный отсчет
+        return "Cчетчик сброшен на начальное значение {}".format(self.current)
 
-my_count=Counter(10, 15)
 
-print(my_count.increment_count())
-print(my_count.increment_count())
-print(my_count.increment_count())
-print(my_count.increment_count())
-print(my_count.refresh_count())
-print(my_count.increment_count())
-print(my_count.increment_count())
+if __name__ == "__main__":
+    my_count = Counter(10, 15)
+    print(my_count.status_count())
+    print(my_count.status_count())
+    print(my_count.refresh_count())
+    print(my_count.status_count())
+    print(my_count.status_count())
+    print(my_count.status_count())
